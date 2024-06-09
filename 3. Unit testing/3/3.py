004e3fe46d72ddd5f5bdc94b0a1435f1e9d5ca3c
Di <71814987+Dinara58@users.noreply.github.com>
2024-06-09 15:47:32 +0500

    Update 3.py

--- 3. Unit testing/3/3.py
+++ 3. Unit testing/3/3.py
@@ -6,11 +6,11 @@ def is_correct_mobile_phone_number_ru(number):
 
 def test_is_correct_mobile_phone_number_ru():
     test_cases = {
-        "+7 900 123-45-67": True,
-        "8(910)1234567": True,
-        "7 800 1234567": False,
-        "+7 999 123456": False,
-        "8 800 12345678": False
+        "+7 937 587-47-76": True,
+        "8(920)4321587": True,
+        "7 900 7654321": False,
+        "+7 995 654321": False,
+        "8 800 55543587": False
     }
 
     for test_case, expected_result in test_cases.items():
